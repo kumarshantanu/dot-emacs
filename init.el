438063b0dfca29541fbf91573b707455699ffb3f
Shantanu Kumar <kumar.shantanu@gmail.com>
2024-12-06 21:47:31 +0530

    add lsp-mode

--- init.el
+++ init.el
@@ -126,6 +126,25 @@
   :init
   (add-hook 'clojure-mode-hook 'enable-paredit-mode))
 
+;; LSP-mode
+(use-package clojure-mode)
+(use-package lsp-mode)
+;(use-package cider)
+(use-package lsp-treemacs)
+(use-package flycheck)
+(use-package company)
+
+(add-hook 'clojure-mode-hook 'lsp)
+(add-hook 'clojurescript-mode-hook 'lsp)
+(add-hook 'clojurec-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-minimum-prefix-length 1
+      ; lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
+      ; lsp-enable-completion-at-point nil ; uncomment to use cider completion instead of lsp
+      )
 
 ;; invoke one (ONLY one) of CIDER vs inf-clojure
 (add-to-list 'load-path (concat user-emacs-directory "/clojure/"))
